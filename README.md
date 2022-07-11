3db2aa5da31ac23ae543ad824e459b14501cdfb4
richa130 <richabavadekar@me.com>
2022-07-11 12:53:35 -0700

    Complete JS logic for rps game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This project solidified basic JavaScript topics.
+
+The first part of the project did not include any GUI, just the basic underlying JS logic. I used functions, if statements, switch statements, for loops, etc. to make the logic following the rules of Rock, Paper, Scissors.
--- rps.js
+++ rps.js
@@ -19,44 +19,74 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase(); // might not work cause strings are immutable in JS?
-
     if(playerSelection == "rock") {
         if(computerSelection == "rock") {
-            return "Tie! Rock and rock.";
+            return "Tie";
         }
         if(computerSelection == "paper") {
-            return "You lose! Paper beats rock.";
+            return "Lose";
         }
         if(computerSelection == "scissors") {
-            return "You win! Rock beats scissors.";
+            return "Win";
         }
     }
     else if(playerSelection == "paper") {
         if(computerSelection == "rock") {
-            return "You win! Paper beats rock.";
+            return "Win";
         }
         if(computerSelection == "paper") {
-            return "Tie! Paper and paper.";
+            return "Tie";
         }
         if(computerSelection == "scissors") {
-            return "You lose! Scissors beats paper.";
+            return "Lose";
         }
 
     }
     else if(playerSelection == "scissors") {
         if(computerSelection == "rock") {
-            return "You lose! Rock beats scissors.";
+            return "Lose";
         }
         if(computerSelection == "paper") {
-            return "You win! Scissors beats paper.";
+            return "Win";
         }
         if(computerSelection == "scissors") {
-            return "Tie! Scissors and scissors."
+            return "Tie"
         }
     }
 }
 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection;
+    let computerSelection;
+    let result;
 
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Pick a choice (rock, paper, or scissors): ").toLowerCase();
+        computerSelection = computerPlay();
 
+        result = playRound(playerSelection, computerSelection);
 
+        if(result == "Win") {
+            playerScore++;
+        }
+        else if(result == "Lose") {
+            computerScore++;
+        }
+
+        console.log(`Round ${i}: ${result}! You picked ${playerSelection} and the computer picked ${computerSelection}.`);
+    }
+
+    if(playerScore > computerScore) {
+        console.log(`Good Job! You won with a score of ${playerScore}.`);
+    }
+    else if(playerScore == computerScore) {
+        console.log(`You tied with a score of ${playerScore}. At least you didn't lose?`);
+    }
+    else {
+        console.log(`You lose with a score of ${playerScore}. Better luck next time!`);
+    }
+}
+
+game();
